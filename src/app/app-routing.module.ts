530c8871290d11b49d88afe3d982c6912eba3203
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-02-04 23:02:10 +0200

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './About/About.component';
+import { ContactComponent } from './Contact/Contact.component';
+import { PortfolioComponent } from './Portfolio/Portfolio.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { HomeComponent } from './Home/Home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent,
+  },
+  {
+    path: '**',
+    component: NopagefoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './About/About.component';
+import { FooterComponent } from './Footer/Footer.component';
+import { NavbarComponent } from './Navbar/Navbar.component';
+import { ContactComponent } from './Contact/Contact.component';
+import { PortfolioComponent } from './Portfolio/Portfolio.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { HomeComponent } from './Home/Home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [							
+    AppComponent,
+      AboutComponent,
+      FooterComponent,
+      NavbarComponent,
+      ContactComponent,
+      PortfolioComponent,
+      NopagefoundComponent,
+      HomeComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
