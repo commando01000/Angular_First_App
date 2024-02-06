51c0e802a47da281957c3a4ade62cd00847d3c3e
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-02-06 18:51:26 +0200

    added footer and some UI

--- src/app/Navbar/Navbar.component.ts
+++ src/app/Navbar/Navbar.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare var $: any; // Declare the jQuery variable
 
 @Component({
   selector: 'app-Navbar',
   templateUrl: './Navbar.component.html',
-  styleUrls: ['./Navbar.component.css']
+  styleUrls: ['./Navbar.component.css'],
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, AfterViewInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  ngAfterViewInit() {
+    $('ul.navbar-nav li a').click(function(this: HTMLAnchorElement) {
+      console.log($(this).prop('outerHTML'));
+      $('ul.navbar-nav li a').removeClass('active');
+      $(this).addClass('active');
+    });
+    $('a.navbar-brand').click(function(this: HTMLAnchorElement) {
+      console.log($(this).prop('outerHTML'));
+      $('ul.navbar-nav li a').removeClass('active');
+    });
   }
-
 }
