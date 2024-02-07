728e12a18bfb64a4e5a75392fdff9d5ed30da9de
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-02-07 20:53:00 +0200

    .

--- src/app/Navbar/Navbar.component.ts
+++ src/app/Navbar/Navbar.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  HostBinding,
+  HostListener,
+} from '@angular/core';
 declare var $: any; // Declare the jQuery variable
 
 @Component({
@@ -8,19 +14,37 @@ declare var $: any; // Declare the jQuery variable
 })
 export class NavbarComponent implements OnInit, AfterViewInit {
   constructor() {}
-  navList:any;
+
+  navList: any;
+  isScrolled: boolean = false;
+
   ngOnInit() {}
+  @HostBinding('class.scrolled')
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: any) {
+    const scrollOffset =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+
+    const scrollThreshold = 100;
+
+    this.isScrolled = scrollOffset >= scrollThreshold;
+  }
   ngAfterViewInit() {
-    $('ul.navbar-nav li a').click(function(this: any) {
-      console.log($(this).prop('outerHTML'));
-      $('ul.navbar-nav li a').removeClass('active');
-      $(this).addClass('active');
-    });
-    $('a.navbar-brand').click(function(this: any) {
-      console.log($(this).prop('outerHTML'));
-      $('ul.navbar-nav li a').removeClass('active');
-    });
-    this.navList = document.querySelectorAll('ul.navbar-nav li a');
+
+    // $('ul.navbar-nav li a').click(function (this: any) {
+    //   console.log($(this).prop('outerHTML'));
+    //   $('ul.navbar-nav li a').removeClass('active');
+    //   $(this).addClass('active');
+    // });
+    // $('a.navbar-brand').click(function (this: any) {
+    //   console.log($(this).prop('outerHTML'));
+    //   $('ul.navbar-nav li a').removeClass('active');
+    // });
+    
+    // this.navList = document.querySelectorAll('ul.navbar-nav li a');
     // this.x.forEach((element: any) => {
     //   element.addEventListener('click', function(e: any) {
     //     console.log(e.target);
