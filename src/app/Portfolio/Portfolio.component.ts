76ccbfc6af30c58ab2a1c764d82de770e9b07503
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2024-02-07 23:05:35 +0200

    .

--- src/app/Portfolio/Portfolio.component.ts
+++ src/app/Portfolio/Portfolio.component.ts
@@ -7,18 +7,37 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit, AfterViewInit {
   items: any;
-
+  myImage: any;
+  myCard: any;
   constructor() {}
 
   ngOnInit() {}
 
   ngAfterViewInit() {
     this.items = document.querySelectorAll('.item');
+    this.myCard = document.querySelector('.my-card');
+    console.log(this.myCard);
+
     console.log(this.items);
-    this.items.forEach((item: any) => {
-      item.addEventListener('click', (e: any) => {
-        console.log(e.target);
+    this.items.forEach((element: any) => {
+      element.addEventListener('click', (e: any) => {
+        e.stopPropagation();
+        console.log(element);
+        console.log(element.querySelector('img').getAttribute('src'));
+        this.myImage = element.querySelector('img').getAttribute('src');
+        this.myCard.classList.remove('d-none');
+        this.myCard.classList.add('d-flex');
+        console.log(this.myCard);
       });
+    });
+    // Add a global event listener to handle clicks anywhere in the document
+    document.addEventListener('click', (e: any) => {
+      if (!this.myCard.contains(e.target)) {
+        this.myCard.classList.remove('d-flex');
+        this.myCard.classList.add('d-none');
+        console.log("GGGGG");
+        
+      }
     });
   }
 }
